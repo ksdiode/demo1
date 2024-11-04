7688991533c4b53db711aad633bd170be0bc7597
Gu Su Kim <gusukim@gmail.com>
2024-11-04 14:26:12 +0900

    h2 test

--- src/test/java/org/scoula/demo1/Demo1ApplicationTests.java
+++ src/test/java/org/scoula/demo1/Demo1ApplicationTests.java
@@ -2,8 +2,11 @@ package org.scoula.demo1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application-dev.properties")
 class Demo1ApplicationTests {
 
   @Test
