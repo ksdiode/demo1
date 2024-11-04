79f932add34d1c7deb0dcd8bad9e73a8bedf2022
Gu Su Kim <gusukim@gmail.com>
2024-11-04 15:17:45 +0900

    HomeController update

--- src/main/java/org/scoula/demo1/controller/HomeController.java
+++ src/main/java/org/scoula/demo1/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
   @GetMapping()
   public String home() {
-    return "hello world";
+    return "hello world  ===> ";
   }
 }
